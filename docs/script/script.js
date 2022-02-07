cedb193394a023bdeb1f336e134d19b68c1ee549
Adaeze Ndupu <andupu4@gmail.com>
2022-02-07 03:01:07 +0100

    Adjust blur for firefox

--- docs/script/script.js
+++ docs/script/script.js
@@ -4,10 +4,25 @@ const closeBtn = document.querySelector("[data-close-btn]");
 
 menuBtn.addEventListener("click", () => {
   navlist.classList.toggle("open");
+  
+  if(navlist.classList.contains("open")){
+    menuBtn.style.display = "none"
+  }
+  else{
+    menuBtn.style.display = "block"
+  }
+  
 });
 
 closeBtn.addEventListener("click", () => {
   navlist.classList.toggle("open");
+
+  if(navlist.classList.contains("open")){
+    menuBtn.style.display = "none"
+  }
+  else{
+    menuBtn.style.display = "block"
+  }
 });
 
 window.addEventListener("resize",()=>{
