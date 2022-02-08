72a64fe9b7fe0fe8ef9b394d1aaa36c6bf96e572
Adaeze Ndupu <andupu4@gmail.com>
2022-02-08 20:39:48 +0100

    Added destination pages

--- docs/script/script.js
+++ docs/script/script.js
@@ -25,35 +25,53 @@ closeBtn.addEventListener("click", () => {
   }
 });
 
+var url = window.location.href.split('/')[4];
+
+
 window.addEventListener("resize", changeBackground)
 
 function changeBackground(){
-  if (window.matchMedia("(max-width: 767px)").matches) {
-    document.getElementById("background-img").src = "./assets/home/background-home-tablet.jpg";
-  } else if(window.matchMedia("(min-width: 768px) and (max-width: 1151.9px)").matches) {
-    document.getElementById("background-img").src = "./assets/home/background-home-mobile.jpg";
+  if(url === 'index.html' || url === 'index.html#'){
+    if (window.matchMedia("(max-width: 767px)").matches) {
+      document.getElementById("background-img").src = "./assets/home/background-home-tablet.jpg";
+    } else if(window.matchMedia("(min-width: 768px) and (max-width: 1151.9px)").matches) {
+      document.getElementById("background-img").src = "./assets/home/background-home-mobile.jpg";
+    }
+    else{
+      document.getElementById("background-img").src = "./assets/home/background-home-desktop.jpg";
+    }
   }
-  else{
-    document.getElementById("background-img").src = "./assets/home/background-home-desktop.jpg";
+  if(url === 'destination-moon.html' || url === 'destination-mars.html' || url === 'destination-europa.html' || url === 'destination-titan.html'){
+    if (window.matchMedia("(max-width: 767px)").matches) {
+      document.getElementById("background-img").src = "./assets/destination/background-destination-tablet.jpg";
+    } else if(window.matchMedia("(min-width: 768px) and (max-width: 1151.9px)").matches) {
+      document.getElementById("background-img").src = "./assets/destination/background-destination-mobile.jpg";
+    }
+    else{
+      document.getElementById("background-img").src = "./assets/destination/background-destination-desktop.jpg";
+    }
   }
+
 }
 document.addEventListener("DOMContentLoaded",changeBackground)
-document.addEventListener("DOMContentLoaded", function () {
-  const selector = ".tablet-navlink";
-  const tabLinks = Array.from(document.querySelectorAll(selector));
-  const nav = document.querySelector(".tablet-navlist-items");
 
-  function makeActive(evt) {
-    const target = evt.target;
+// document.addEventListener("DOMContentLoaded", function () {
+//   const selector = ".tablet-navlink";
+//   const tabLinks = Array.from(document.querySelectorAll(selector));
+//   const nav = document.querySelector(".tablet-navlist-items");
 
-    if (!target || !target.matches(selector)) {
-      return;
-    }
+//   function makeActive(evt) {
+//     const target = evt.target;
 
-    tabLinks.forEach((elem) => elem.classList.remove("active"));
+//     if (!target || !target.matches(selector)) {
+//       return;
+//     }
 
-    evt.target.classList.add("active");
-  }
+//     tabLinks.forEach((elem) => elem.classList.remove("active"));
 
-  nav.addEventListener("mousedown", makeActive);
-});
+//     evt.target.classList.add("active");
+//   }
+  
+
+//   nav.addEventListener("mousedown", makeActive);
+// });
